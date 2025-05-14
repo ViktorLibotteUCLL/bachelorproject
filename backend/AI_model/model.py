26466ff8f18555e1fb93cceb74d62c9ff35101a4
ViktorLibotteUCLL <113979597+ViktorLibotteUCLL@users.noreply.github.com>
2025-05-14 14:06:20 +0200

    model.py changes

--- backend/AI_model/model.py
+++ backend/AI_model/model.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
 
-results = model.train(data="datasets/data.yaml", epochs=30, device="cpu", imgsz=640)
+def main():
+    model = YOLO("yolov8n.pt")
+    results = model.train(data="datasets/data.yaml", epochs=30, imgsz=640)
+    val_results = model.val()
+    exported_model = model.export(format="onnx")
 
-val_results = model.val()
 
-exported_model = model.export(format="onnx")
+if __name__ == "__main__":
+    main()
