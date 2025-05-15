a0ee31e7d4ada567c7c4234e30404bbd64c0f061
ViktorLibotteUCLL <113979597+ViktorLibotteUCLL@users.noreply.github.com>
2025-05-15 13:34:50 +0200

    Loading screen (#16)
    * set camera flash to auto
    
    * added loading screen and removed old navigation
    
    * fixed poopy git merge error

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -9,6 +9,7 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { useState } from "react";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -25,7 +26,6 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
