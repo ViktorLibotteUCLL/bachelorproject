d06a32f9c39ae35cf43e37a5c5411de8ebd7a709
Rune-VanBouwelBrams <rune.vanbouwelbrams@student.ucll.be>
2025-05-15 15:15:02 +0200

    Added multiple images for splashscreen + animated them done

--- frontend/app/Splashscreen.tsx
+++ frontend/app/Splashscreen.tsx
@@ -1,41 +1,65 @@
 import { transform } from "@babel/core";
 import React from "react";
-import {View, StyleSheet, Animated} from "react-native";
+import { View, StyleSheet, Animated } from "react-native";
 
 const SplashScreen = () => {
-    const imageScale = new Animated.Value(0.1);
-    // const imagePosistion = new Animated.Value(-200);
+    const imageScale = new Animated.Value(0);
+    const imagePosistionB = new Animated.ValueXY({ x: 150, y: 0 });
+    const imagePosistionRaillo = new Animated.ValueXY({ x: -150, y: 0 });
 
-    Animated.parallel([
-        Animated.timing(imageScale, {
-            toValue: 1,
+    Animated.sequence([
+        Animated.timing(imagePosistionB, {
+            toValue: { x: 75, y: 0 },
             duration: 1000,
             useNativeDriver: true,
         }),
-        // Animated.timing(imagePosistion, {
-        //     toValue: 0,
-        //     duration: 1000,
-        //     useNativeDriver: true,
-        // }),
+        Animated.parallel([
+            Animated.timing(imageScale, {
+                toValue: 1,
+                duration: 1000,
+                useNativeDriver: true,
+            }),
+            Animated.timing(imagePosistionRaillo, {
+                toValue: { x: -45, y: 0 },
+                duration: 1000,
+                useNativeDriver: true,
+            }),
+        ]),
     ]).start();
 
     return (
         <View style={styles.container}>
-            <Animated.Image
-                source={require("../assets/images/Braillo_splashscreen_lightmode.png")}
-                style={[
-                    styles.image,
-                    {
-                        transform: [
-                            { scale: imageScale },
-                            //{ translateY: imagePosistion },
-                        ],
-                    }
-                ]}
-            />
+            <View style={styles.imageRow}>
+                <Animated.Image
+                    source={require("../assets/images/Splashscreen_B_lightmode_no_bg.png")}
+                    style={[
+                        styles.image,
+                        {
+                            transform: [
+                                //{ scale: imageScale },
+                                { translateX: imagePosistionB.x },
+                                { translateY: imagePosistionB.y },
+                            ],
+                        },
+                    ]}
+                />
+                <Animated.Image
+                    source={require("../assets/images/Splashscreen_raillo_lightmode_no_bg.png")}
+                    style={[
+                        styles.image,
+                        {
+                            transform: [
+                                { translateX: imagePosistionRaillo.x },
+                                { translateY: imagePosistionRaillo.y },
+                                { scale: imageScale },
+                            ],
+                        },
+                    ]}
+                />
+            </View>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
@@ -43,6 +67,11 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#fff",
+    },
+    imageRow: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
     },
     image: {
         width: 200,
