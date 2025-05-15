fce0e802ce9b8a74e840f62e28df529192956e9a
Rune-VanBouwelBrams <91271491+Rune-VanBouwelBrams@users.noreply.github.com>
2025-05-15 15:31:42 +0200

    9 animated splash screen (#19)
    * Added splashscreen graphics
    
    * Changed splashscreen logo app.json
    
    * Added splashscreen animations
    
    * Added splashscreens with no background
    
    * Changed splashscreen
    
    * Added multiple images for splashscreen + animated them done
    
    * Made timeout longer
    
    * Changed timeout again
    
    * splashscreen is more centred now
    
    * Made random bar at top of screen disappear

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,21 +1,43 @@
 import {
   DarkTheme,
   DefaultTheme,
+  NavigationContainer,
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
+import { use, useCallback, useEffect, useState } from 'react';
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import SplashScreen from "./Splashscreen";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { useState } from "react";
+
+// SplashScreen.preventAutoHideAsync();
+
+// SplashScreen.setOptions({
+//   duration: 1000,
+//   fade: true,
+// });
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [loading, setLoading] = useState(true);
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+  }, []);
+
+  useEffect(() => {
+    if (loaded) {
+      //SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
   if (!loaded) {
     // Async font loading only occurs in development.
@@ -24,11 +46,17 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      {loading? (
+        <SplashScreen />
+      ) : (
+        <>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="+not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </>
+      )}
     </ThemeProvider>
   );
 }
