7488e5415f62ef7bf7b24d7422225d4553cc8c72
MartheDHoogheUCLL <marthe.dhooghe@student.ucll.be>
2025-05-16 10:04:52 +0200

    output in correct order

--- backend/AI_model/predict.py
+++ backend/AI_model/predict.py
@@ -1,9 +1,17 @@
 from ultralytics import YOLO
 
-imagefile = "C://Users//Marthe//Downloads//image4.jpg"
+imagefile = "C://Users//Marthe//Downloads//image6.jpg"
 model = YOLO(
     "C://Users//Marthe//Documents//school//bp//bachelorproject//backend//runs//detect//train14//weights//best.pt"
 )
+
+
+def coord_y(i):
+    return i[2]
+
+
+def coord_x(i):
+    return i[1]
 
 
 def get_instances(image):
@@ -18,10 +26,15 @@ def get_instances(image):
         label = class_names[class_id]
         # print(label)
         xyxy = b.xyxy[0][:2].tolist()
-        data = [label, round(xyxy[0] / 100), round(xyxy[1] / 100)]
+        data = [label, round(xyxy[0] / 50), round(xyxy[1] / 50)]
         output.append(data)
-        # print(label)
 
+        output.sort(reverse=False, key=coord_y)
+        output.sort(reverse=False, key=coord_x)
+
+        # print(label)
+    result.show()
+    result.save(filename="result.jpg")
     return output
 
 
