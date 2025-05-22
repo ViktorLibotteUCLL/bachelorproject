309a41955788ce8e79cb1a6aa52d4699b64b4ab2
MartheDHoogheUCLL <marthe.dhooghe@student.ucll.be>
2025-05-22 17:04:16 +0200

    vernieuwde parameters

--- backend/AI_model/model.py
+++ backend/AI_model/model.py
@@ -4,11 +4,12 @@ from ultralytics import YOLO
 def main():
     model = YOLO("yolo11n.pt")
 
-    results = model.train(data="datasets/data.yaml", epochs=15, imgsz=320)
-
+    results = model.train(
+        data="datasets/data.yaml", epochs=200, patience=50, name="half_cleaned"
+    )
+    print(results)
     val_results = model.val()
-
-    exported_model = model.export(format="onnx")
+    print(val_results)
 
 
 if __name__ == "__main__":
