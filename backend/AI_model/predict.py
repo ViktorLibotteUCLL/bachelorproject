21d33c6120903f2eacf3a43e4d28411880f28967
Marie Bastiaens <r0891122@ucll.be>
2025-05-28 13:26:01 +0200

    removed old comments

--- backend/AI_model/predict.py
+++ backend/AI_model/predict.py
@@ -7,30 +7,6 @@ model = YOLO(
 )
 
 
-# def get_instances(image):
-#     results = model(image)
-#     # print(type(results[0]))
-#     result = results[0]
-#     class_names = model.names
-#     output = []
-#     for b in result.boxes:
-#         # print(b)
-#         class_id = int(b.cls[0])
-#         label = class_names[class_id]
-#         # print(label)
-#         xyxy = b.xyxy[0][:2].tolist()
-#         data = [label, round(xyxy[0] / 50), round(xyxy[1] / 50)]
-#         output.append(data)
-
-#         output.sort(reverse=False, key=coord_y)
-#         output.sort(reverse=False, key=coord_x)
-
-#         # print(label)
-#     result.show()
-#     result.save(filename="result.jpg")
-#     return output
-
-
 def get_instances(image):
     results = model(image)[0]
     results.show()
