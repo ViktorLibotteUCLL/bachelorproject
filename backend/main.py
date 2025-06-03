717b9b227f0fd48d3f6700128d245d020d44d52a
ViktorLibotteUCLL <113979597+ViktorLibotteUCLL@users.noreply.github.com>
2025-06-03 09:51:22 +0200

    fix main

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from fastapi.responses import JSONResponse
 import os
 from dotenv import load_dotenv
 from PIL import Image, ExifTags
+import math
 
 load_dotenv()
 
@@ -16,91 +17,26 @@ from ultralytics import YOLO
 
 model = YOLO(
     # "C://Users//viktorLibotte//Desktop//bachelorproject//backend//runs//detect//train_1605//best.pt"
-    "C://Users//viktorLibotte//Desktop//bachelorproject//backend//runs//detect//train_3005//best.pt"
+    "C:/Users/basti/Documents/School/3de_jaar/Bachelorproef/bachelorproject/backend/runs/detect/train2705/best.pt"
 )
 
 
-def coord_y(i):
-    return i[2]
-
-
-def coord_x(i):
-    return i[1]
-
-
 def get_instances(image):
-    results = model(image)
-    # print(type(results[0]))
-    result = results[0]
+    results = model(image)[0]
+    results.show()
+    results.save(filename="result.jpg")
     class_names = model.names
     output = []
-    for b in result.boxes:
-        # print(b)
-        class_id = int(b.cls[0])
+    i = sorted(
+        results.boxes.data.tolist(),
+        key=lambda result: (math.floor(result[1] / 500), result[0]),
+    )
+    for result in i:
+        x1, y1, x2, y2, score, class_id = result
         label = class_names[class_id]
-        # print(label)
-        xyxy = b.xyxy[0][:2].tolist()
-        data = [label, round(xyxy[0] / 50), round(xyxy[1] / 50)]
-        output.append(data)
-
-        output.sort(reverse=False, key=coord_y)
-        output.sort(reverse=False, key=coord_x)
-
-        # print(label)
-    result.show()
-    result.save(filename="result.jpg")
+        output.append([label, x1, y1])
+    # print(output)
     return output
-
-
-# import torch
-# import torchvision.ops as ops
-
-
-# def apply_nms(boxes, scores, iou_threshold=0.5):
-#     return ops.nms(boxes, scores.to(boxes.device), iou_threshold)
-
-
-# def get_instances(image):
-#     results = model(image)
-#     result = results[0]
-#     class_names = model.names
-#     output = []
-
-#     boxes = []
-#     scores = []
-#     labels = []
-
-#     for b in result.boxes:
-#         class_id = int(b.cls[0])
-#         label = class_names[class_id]
-#         xyxy = b.xyxy[0]
-#         score = b.conf[0]
-
-#         boxes.append(xyxy)
-#         scores.append(score)
-#         labels.append(label)
-
-#     boxes = torch.stack(boxes)
-#     scores = torch.tensor(scores)
-
-#     keep = apply_nms(boxes, scores, iou_threshold=0.5)
-
-#     for i in keep:
-#         xyxy = boxes[i][:2].tolist()
-#         x = round(xyxy[0])
-#         y = round(xyxy[1])
-#         output.append((labels[i], x, y))
-
-#     # Sort in reading order
-#     def reading_order_key(item):
-#         label, x, y = item
-#         return (round(y / 50), x)
-
-#     output.sort(key=reading_order_key)
-
-#     result.show()
-#     result.save(filename="result.jpg")
-#     return output
 
 
 @app.middleware("http")
