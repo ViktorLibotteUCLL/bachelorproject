710a23c196104a36274b3002583fa68b0a3b0a6d
MartheDHoogheUCLL <marthe.dhooghe@student.ucll.be>
2025-06-06 10:32:53 +0200

    first try at inserting spaces

--- backend/AI_model/predict.py
+++ backend/AI_model/predict.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 import math
 
-imagefile = "C:/Users/basti/Downloads/image2.jpg"
+imagefile = "C:/Users/Marthe/Downloads/image6.jpg"
 model = YOLO(
-    "C:/Users/basti/Documents/School/3de_jaar/Bachelorproef/bachelorproject/backend/runs/detect/train2705/best.pt"
+    "C:/Users/Marthe/Documents/school/bp/bachelorproject/backend/runs/detect/train14/weights/best.pt"
 )
 
 
@@ -13,17 +13,27 @@ def get_instances(image):
     results.save(filename="result.jpg")
     class_names = model.names
     output = []
-    i = sorted(results.boxes.data.tolist(), key=lambda result: (math.floor(result[1]/500), result[0]))
-    for result in i:
+    # midpoints = []
+    sorted_list = sorted(
+        results.boxes.data.tolist(),
+        key=lambda result: (math.floor(result[1] / 500), result[0]),
+    )
+    for result in sorted_list:
         x1, y1, x2, y2, score, class_id = result
         label = class_names[class_id]
-        output.append([label, x1, y1])
-    #print(output)
+        output.append([label, x1, y1, x2, y2])
+        # midpoints.append([label, x1 + x2 / 2, y1 + y2 / 2])
+    if len(sorted_list) != 0:
+
+        avg_width = sum((r[2] - r[0]) for r in sorted_list) / len(sorted_list)
+        print("avg width:", avg_width)
+        for index in range(1, len(sorted_list)):
+            if (output[index][1] - output[index - 1][1]) > (2 * avg_width):
+                output.insert(index - 1, [" "])
+
+    print(output)
+    print([str(t[0]) for t in output])
     return output
-    
-    
-            
-    
 
 
 get_instances(imagefile)
