2590bb03169127b70f2396c79756808d7df1d856
MartheDHoogheUCLL <marthe.dhooghe@student.ucll.be>
2025-06-06 10:36:47 +0200

    finishing adding spaces

--- backend/AI_model/predict.py
+++ backend/AI_model/predict.py
@@ -21,19 +21,20 @@ def get_instances(image):
     for result in sorted_list:
         x1, y1, x2, y2, score, class_id = result
         label = class_names[class_id]
-        output.append([label, x1, y1, x2, y2])
+        output.append([label, x1, y1])
         # midpoints.append([label, x1 + x2 / 2, y1 + y2 / 2])
     if len(sorted_list) != 0:
-
+        output_spaces = [output[0]]
         avg_width = sum((r[2] - r[0]) for r in sorted_list) / len(sorted_list)
         print("avg width:", avg_width)
         for index in range(1, len(sorted_list)):
             if (output[index][1] - output[index - 1][1]) > (2 * avg_width):
-                output.insert(index - 1, [" "])
+                output_spaces.append(" ")
+            output_spaces.append(output[index])
 
-    print(output)
-    print([str(t[0]) for t in output])
-    return output
+    print(output_spaces)
+    print([str(t[0]) for t in output_spaces])
+    return output_spaces
 
 
 get_instances(imagefile)
