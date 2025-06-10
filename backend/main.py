7bb7874380a4f87e68f58418c6bca7ddb614f47d
ViktorLibotteUCLL <113979597+ViktorLibotteUCLL@users.noreply.github.com>
2025-06-10 10:08:03 +0200

    fixed output sorting

--- backend/main.py
+++ backend/main.py
@@ -28,10 +28,17 @@ def get_instances(image):
     results.save(filename="result.jpg")
     class_names = model.names
     output = []
+    if len(results.boxes.data.tolist()) != 0:
+        avg_width = sum((r[2] - r[0]) for r in results.boxes.data.tolist()) / len(
+            results.boxes.data.tolist()
+        )
+        avg_height = sum((r[3] - r[1]) for r in results.boxes.data.tolist()) / len(
+            results.boxes.data.tolist()
+        )
     # midpoints = []
     sorted_list = sorted(
         results.boxes.data.tolist(),
-        key=lambda result: (math.floor(result[1] / 500), result[0]),
+        key=lambda result: (math.floor(result[1] / (avg_height / 2)), result[0]),
     )
     for result in sorted_list:
         x1, y1, x2, y2, score, class_id = result
@@ -41,7 +48,7 @@ def get_instances(image):
     # midpoints.append([label, x1 + x2 / 2, y1 + y2 / 2])
     if len(sorted_list) != 0:
         output_spaces = [output[0]]
-        avg_width = sum((r[2] - r[0]) for r in sorted_list) / len(sorted_list)
+        # avg_width = sum((r[2] - r[0]) for r in sorted_list) / len(sorted_list)
         # print("avg width:", avg_width)
         for index in range(1, len(sorted_list)):
             if abs(output[index][1] - output[index - 1][1]) > (2 * avg_width):
